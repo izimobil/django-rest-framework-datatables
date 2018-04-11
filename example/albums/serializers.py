bade3ddc503354deb6a110620919bf9e9b565c39
David Jean Louis <izimobil@gmail.com>
2018-04-11 15:13:44 +0200

    No need to use DatatablesModelSerializer

--- example/albums/serializers.py
+++ example/albums/serializers.py
@@ -1,12 +1,11 @@
-from rest_framework import serializers as drf_serializers
-from rest_framework_datatables import serializers
+from rest_framework import serializers
 
 from .models import Album
 
 
-class AlbumSerializer(serializers.DatatablesModelSerializer):
-    artist_name = drf_serializers.ReadOnlyField(source='artist.name')
-    genres = drf_serializers.SerializerMethodField()
+class AlbumSerializer(serializers.ModelSerializer):
+    artist_name = serializers.ReadOnlyField(source='artist.name')
+    genres = serializers.SerializerMethodField()
 
     def get_genres(self, album):
         return ', '.join([str(genre) for genre in album.genres.all()])
