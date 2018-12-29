0e360ed3d211394628de4519d4573146ec647c85
Eric <32947634+youshouldtellmemore@users.noreply.github.com>
2018-12-28 20:04:15 -0800

    Update example's albums views.py to demonstrate use of the Metaclass option datatables_extra_json. The example use will provide an 'options' key in the JSON including lists of Genre and Artist records in the format expected by Editor's select type.

--- example/albums/views.py
+++ example/albums/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.response import Response
 
-from .models import Album, Artist
+from .models import Album, Artist, Genre
 from .serializers import AlbumSerializer, ArtistSerializer
 
 
@@ -11,9 +11,22 @@ def index(request):
     return render(request, 'albums/albums.html')
 
 
+def get_album_options():
+    return "options", {
+        "artist": [{'label': obj.name, 'value': obj.pk} for obj in Artist.objects.all()],
+        "genre": [{'label': obj.name, 'value': obj.pk} for obj in Genre.objects.all()]
+    }
+
+
 class AlbumViewSet(viewsets.ModelViewSet):
     queryset = Album.objects.all().order_by('rank')
     serializer_class = AlbumSerializer
+
+    def get_options(self):
+        return get_album_options()
+
+    class Meta:
+        datatables_extra_json = ('get_options', )
 
 
 class ArtistViewSet(viewsets.ViewSet):
@@ -23,3 +36,9 @@ class ArtistViewSet(viewsets.ViewSet):
     def list(self, request):
         serializer = self.serializer_class(self.queryset, many=True)
         return Response(serializer.data)
+
+    def get_options(self):
+        return get_album_options()
+
+    class Meta:
+        datatables_extra_json = ('get_options', )
