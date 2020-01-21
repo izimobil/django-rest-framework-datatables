089700a4f8752f5a1528af2372d8e987c3fe0084
Friedrich Delgado <delgado@dfn-cert.de>
2020-01-21 15:21:42 +0100

    start overriding the filter_queryset method
    reimplemented based on DatatablesBaseFilterBackend

--- rest_framework_datatables/django_filters/backends.py
+++ rest_framework_datatables/django_filters/backends.py
@@ -2,6 +2,16 @@ from rest_framework_datatables import filters
 from django_filters.rest_framework.backends import DjangoFilterBackend
 
 
-class DatatablesFilterBackend(filters.DatatablesFilterBackend,
+class DatatablesFilterBackend(filters.DatatablesBaseFilterBackend,
                               DjangoFilterBackend):
-    pass
+
+    def filter_queryset(self, request, queryset, view):
+        if request.accepted_renderer.format != 'datatables':
+            return queryset
+
+        filtered_count_before = self.count_before(queryset, view)
+
+        filterset = self.get_filterset(request, queryset, view)
+        if filterset is None:
+            self.count_after(view, filtered_count_before)
+            return queryset
