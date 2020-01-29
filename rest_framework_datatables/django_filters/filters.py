7e6172bd5b00b165a4e6e1f6eb5ccf6c4a8007ec
Friedrich Delgado <delgado@dfn-cert.de>
2020-01-29 13:45:02 +0100

    factor out global_lookup property

--- rest_framework_datatables/django_filters/filters.py
+++ rest_framework_datatables/django_filters/filters.py
@@ -7,13 +7,17 @@ class GlobalFilter(object):
         """Return a Q-Object for the local search for this column"""
         ret = Q()
         if self.global_search_value:
-            lookup_expr_parts = self.lookup_expr.split('__')
-            new_lookup = ''
-            if lookup_expr_parts:
-                new_lookup = '__'.join([self.field_name]
-                                       + lookup_expr_parts[:-1]
-                                       + ['icontains'])
-            else:
-                new_lookup = '__'.join([self.field_name, 'icontains'])
-            ret = Q(**{new_lookup: self.global_search_value})
+            ret = Q(**{self.global_lookup: self.global_search_value})
+        return ret
+
+    @property
+    def global_lookup(self):
+        ret = self.lookup_expr
+        lookup_expr_parts = self.lookup_expr.split('__')
+        if lookup_expr_parts:
+            ret = '__'.join([self.field_name]
+                            + lookup_expr_parts[:-1]
+                            + ['icontains'])
+        else:
+            ret = '__'.join([self.field_name, 'icontains'])
         return ret
