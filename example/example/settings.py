3d560775718f765ab4d67e8d4c7eedbd7a7b9c95
matthewhegarty <mrhegarty@gmail.com>
2022-11-21 18:12:22 +0000

    modified tox.ini to include deprecation warnings

--- example/example/settings.py
+++ example/example/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 import sys
 
+import django
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, os.path.dirname(BASE_DIR))
@@ -129,7 +131,8 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_L10N = True
+if django.VERSION < (4, 0):
+    USE_L10N = True
 
 USE_TZ = True
 
